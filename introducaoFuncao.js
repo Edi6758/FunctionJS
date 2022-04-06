158fc138a5d9874245b6d0759e7deeaf541998b1
edi6758 <ediarts7@gmail.com>
2022-04-06 08:23:33 -0300

    first commit

---
+++ introducaoFuncao.js
@@ -0,0 +1,14 @@
+//função é um bloco de código nomeado que pode ser chamada onde e quantas vees for necessária
+
+/**
+ * function nomeDaFuncao() {
+ *  <bloco de código/>
+ * }
+ */
+
+function comprimentarPiloto() {
+    console.log("Bom dia piloto")
+}
+
+comprimentarPiloto()
+//vai exibir um console "Bom dia piloto"
