6ead92b7bebd4599e0000284c2e31f37199cffa4
edi6758 <ediarts7@gmail.com>
2022-04-06 17:17:58 -0300

    adicionando arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,13 @@
+//funções anônimas
+
+/**
+ * 
+const dobrarVelocidade = (velocidade) => {
+    return velocidade * 2
+}   
+ */
+
+const dobrarVelocidade = velocidade => velocidade * 2
+
+console.log(dobrarVelocidade(60))
+//120 
